2cb62838e4543532c1e61f4f4d8eaf04b83785c9
fateh mohamed <cr7141414@gmail.com>
2021-01-08 22:05:35 +0100

    add article collection

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ module.exports = ({ env }) => ({
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ffb80b5f-7d66-44fa-be71-6c1f47ec6ce9'
+};
